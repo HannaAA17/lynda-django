49e739d548b69f88671ae2e8255b16314f06136e
Alberto Hanna <alberto.adell17@gmail.com>
2022-10-12 13:28:52 +0200

    dj_esst: notes (list, detail) function views

--- smartnotes/notes/urls.py
+++ smartnotes/notes/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'notes'
 
 urlpatterns = [
-    
+    path('', views.notes_list, name='notes_list_basic'),
+    path('<int:pk>/', views.notes_detail, name='notes_detail_basic'),
 ]
--- smartnotes/notes/views.py
+++ smartnotes/notes/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . import models
 
 # Create your views here.
+def notes_list(request):
+    all_notes = models.Notes.objects.all()
+    return render(request, 'notes/notes_list.html', {'notes':all_notes})
+
+def notes_detail(request, pk):
+    note = get_object_or_404(models.Notes, pk=pk)
+    return render(request, 'notes/notes_detail.html', {'note':note})
